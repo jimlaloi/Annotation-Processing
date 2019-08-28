c4d213f920c5a9aa189e68a6b906fed2cb7f4fca
Jim Law <jimlaw90@gmail.com>
2019-08-27 22:42:09 -0500

    .

--- Annotation PreProcessing.py
+++ Annotation PreProcessing.py
@@ -1,85 +1,99 @@
-# This python script takes as input pairs of excel spreadsheets containing tokens
-# of a lexical unit from Frantext and from MCVF. It combines the data for each
-# LU into an xml file matching the necessary lu.xml format readable by the
-# FrameNet Annotation Tool. Tokens are separated by century within the file.
-# It assigns a sentence ID to each token, concatenating it with the sentence text.
-# It also produces another xml file matching sentid's to source metadata (reference and genre)
+"""
+This python script takes as input pairs of excel spreadsheets containing tokens
+of a lexical unit from Frantext and from MCVF. It combines the data for each
+LU into an xml file matching the necessary lu.xml format readable by the
+FrameNet Annotation Tool. Tokens are separated by century within the file.
+It assigns a sentence ID to each token, concatenating it with the sentence text.
+It also produces another xml file matching sentid's to source metadata (reference and genre)
+"""
 
-# Create dictionary of lexical units and their frames
-ludict = {
-    "coustage":"FR_Spending",
-    "coustageux":"FR_Spending",
-    "coustance":"FR_Spending",
-    "coustement":"FR_Spending",
-    "cout":"FR_Spending",
-    "couter":"FR_Spending",
-    "deboursement":"FR_Spending",
-    "debourser":"FR_Spending",
-    "depense":"FR_Spending",
-    "depenser":"FR_Spending",
-    "encouter":"FR_Spending",
-    "frais":"FR_Spending",
-    "fuer":"FR_Spending",
-    "surcout":"FR_Spending",
-    "couteux":"FR_Spending",
-    "depens":"FR_Spending",
-    "prix":"FR_Spending",
-    "argentdebourse":"FR_Spending",
-    "tirerdesabourse":"FR_Spending",
-    "emploidesonbien":"FR_Spending",
-    "employersonbien":"FR_Spending",
-    "fairedeladepense":"FR_Spending",
-    "fairedesfrais":"FR_Spending",
-    "debourse":"FR_Spending",
-    "cher":"FR_Spending",
-    "dispendieux":"FR_Spending",
-    "onereux":"FR_Spending",
-    "quicoutebeaucoup":"FR_Spending",
-    "quiestdegrandprix":"FR_Spending",
-    "attribuer":"FR_Attributing_cause",
-    "attribution":"FR_Attributing_cause",
-    "explication":"FR_Attributing_cause",
-    "expliquer":"FR_Attributing_cause",
-    "imputable":"FR_Attributing_cause",
-    "imputer":"FR_Attributing_cause",
-    "justifier":"FR_Attributing_cause",
-    "mettresurlecomptede":"FR_Attributing_cause",
-    "aveu":"Reveal_secret",
-    "avouer":"Reveal_secret",
-    "balancer":"Reveal_secret",
-    "cafeter":"Reveal_secret",
-    "confes":"Reveal_secret",
-    "confesser":"Reveal_secret",
-    "confession":"Reveal_secret",
-    "confier":"Reveal_secret",
-    "devoilement":"Reveal_secret",
-    "devoiler":"Reveal_secret",
-    "divulgation":"Reveal_secret",
-    "divulguer":"Reveal_secret",
-    "ebruitement":"Reveal_secret",
-    "ebruiter":"Reveal_secret",
-    "revelation":"Reveal_secret",
-    "reveler":"Reveal_secret",
-    "seconfier":"Reveal_secret",
-    "convenir":"Reveal_secret",
-    "demeurerdaccord":"Reveal_secret",
-    "faireaveu":"Reveal_secret",
-    "denonciation":"Reveal_secret",
-    "denoncer":"Reveal_secret",
-    "fairetomberlevoile":"Reveal_secret",
-    "leverlevoile":"Reveal_secret",
-    "mettreadecouvert":"Reveal_secret",
-    "mettreaujour":"Reveal_secret",
-    "mettreenevidence":"Reveal_secret",
-    "rendrepublic":"Reveal_secret",
-    "eventer":"Reveal_secret",
-    "trahirunsecret":"Reveal_secret",
-    "delateur":"Reveal_secret",
-    "delation":"Reveal_secret",
-}
+import csv
+from lxml import etree
+import re
 
-for x,y in ludict.items():
-    lu = x
-    frame = y
-    # Indicate the directories for each frame where output files for each LU are stored
-    path = f'C:\\Users\\Jim\\OneDrive\\Documents\\Dissertation\\Data\\{frame}\\MCVF data\\{lu}.out'
+# define pretty printing function for xml
+def indent(elem, level=0):
+    i = "\n" + level*"    "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "    "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+# Create dictionary of lexical units and their LU IDs
+luIDs = {'102186': 'coustage', '102187': 'coustageux', '102188': 'coustance', '102189': 'coustement', '1504': 'cout', '1505': 'couter', '1506': 'deboursement', '1507': 'debourser', '1508': 'depense', '1509': 'depenser', '1510': 'encouter', '1511': 'frais', '102190': 'fuer', '102191': 'couteux', '102192': 'depens', '102193': 'prix', '102194': 'argentdebourse', '102195': 'tirerdesabourse', '102196': 'emploidesonbien', '102197': 'employersonbien', '102198': 'fairedeladepense', '102199': 'fairedesfrais', '102200': 'debourse', '102201': 'cher', '102202': 'dispendieux', '102203': 'onereux', '102204': 'quicoutebeaucoup', '102205': 'quiestdegrandprix', '1138': 'attribuer', '1139': 'attribution', '1133': 'explication', '1134': 'expliquer', '1137': 'imputable', '1136': 'imputer', '1135': 'justifier', '1140': 'mettresurlecomptede', '2447': 'aveu', '2448': 'avouer', '2450': 'cafeter', '102206': 'confes', '2451': 'confesser', '2452': 'confession', '2453': 'confier', '2456': 'devoilement', '2457': 'devoiler', '2454': 'divulgation', '2455': 'divulguer', '2462': 'ebruitement', '2463': 'ebruiter', '2459': 'revelation', '2460': 'reveler', '2461': 'seconfier', '102207': 'convenir', '102208': 'demeurerdaccord', '102209': 'denonciation', '102210': 'denoncer', '102211': 'fairetomberlevoile', '102212': 'leverlevoile', '102213': 'mettreadecouvert', '102214': 'mettreaujour', '102215': 'mettreenevidence', '102216': 'rendrepublic', '102217': 'eventer', '102218': 'trahirunsecret', '102219': 'delateur', '102220': 'delation'}
+
+# populate scrubbed LU files with selected sentences...
+for luID in luIDs:
+    centuries = {"20th","19th","18th","17th","16th","15th","14th","13th","12th"}
+    # Set variable for namespace to use when calling tags
+    ns="{http://framenet.icsi.berkeley.edu}"
+    # Read in the selected data from the combined corpus
+    csv_reader = csv.DictReader(open("Combined data preannotation.csv", encoding="utf-8-sig"))
+    # Import scrubbed xml
+    tree = etree.parse(f"../../LU annotation files/scrubbed_lu{luID}.xml")
+    root=tree.getroot()
+    # add sentence node for each token in appropriate century
+    for row in csv_reader:
+        for century in centuries:
+            if (row['LU']==luIDs.get(luID)) and (row['Century']==century):
+                sentence = etree.Element("sentence") # Create new sentence node
+                sentence.set('ID',row['SentID']) # Add SentID as sentence attribute
+                senttext = etree.Element("text") # Create new text node
+                senttext.text = str(row['Text']) # Add sentence text to the node
+                sentence.append(senttext) # Append sentence text as child of sentence node
+                sentAnnoSet = etree.Element("annotationSet") # Create new annotationSet node
+                sentAnnoSet.set('status','MANUAL') # Add status as annotationSet attribute
+                TargetLayer = etree.Element("layer") # Create new target layer node
+                TargetLayer.set('rank','1')
+                TargetLayer.set('name','Target')
+                TargetLabel1 = etree.Element("label") # Create first target label
+                TargetLabel1.set('end',row['Target1End'])
+                TargetLabel1.set('start',row['Target1Start'])
+                TargetLabel1.set('name','Target')
+                TargetLayer.append(TargetLabel1) # Append label 1 as child of Target layer
+                if row['Target2Start'] != "":
+                    TargetLabel2 = etree.Element("label") # Create second target label
+                    TargetLabel2.set('end',row['Target2End'])
+                    TargetLabel2.set('start',row['Target2Start'])
+                    TargetLabel2.set('name','Target')
+                    TargetLayer.append(TargetLabel2) # Append label 2 as child of Target layer
+                if row['Target3Start'] != "":
+                    TargetLabel3 = etree.Element("label") # Create third target label
+                    TargetLabel3.set('end',row['Target3End'])
+                    TargetLabel3.set('start',row['Target3Start'])
+                    TargetLabel3.set('name','Target')
+                    TargetLayer.append(TargetLabel3) # Append label 3 as child of Target layer
+                sentAnnoSet.append(TargetLayer) # Append Target layer as child of annotationSet
+                FELayer = etree.Element("layer") # Create new Frame Element layer node
+                FELayer.set('rank','1')
+                FELayer.set('name','FE')
+                sentAnnoSet.append(FELayer) # Append FE layer as child of annotationSet
+                GFLayer = etree.Element("layer") # Create new Grammatical Function layer node
+                GFLayer.set('rank','1')
+                GFLayer.set('name','GF')
+                sentAnnoSet.append(GFLayer) # Append GF layer as child of annotationSet
+                PTLayer = etree.Element("layer") # Create new Phrase Type layer node
+                PTLayer.set('rank','1')
+                PTLayer.set('name','PT')
+                sentAnnoSet.append(PTLayer) # Append PT layer as child of annotationSet
+                sentence.append(sentAnnoSet) # Append annotationSet as child of sentence node
+                centuryname=f"{century} century" # Identify subCorpus name
+                root.findall(f"{ns}subCorpus[@name='%s']" % centuryname)[0].append(sentence) # Append sentence node as child of subCorpus node
+    # pretty print
+    indent(root)
+    # Write to new file
+    tree.write(f"../../LU annotation files/unannotated_lu{luID}.xml", encoding="utf-8", pretty_print=True)
+    # Add the declaration to the beginning of the file
+    with open(f"../../LU annotation files/unannotated_lu{luID}.xml", 'r+', encoding="UTF-8") as f:
+        content = f.read()
+        f.seek(0, 0)
+        declaration = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
+        f.write(declaration.rstrip('\r\n') + '\n' + content)
